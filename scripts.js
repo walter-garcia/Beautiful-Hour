9b1e85cecb178191ac25186dee2b1c8c5af01b2b
Walter Marcel Barbosa Garcia <waltermbgarcia@hotmail.com>
2019-08-21 16:54:36 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function load() {
+  let message = document.querySelector("div.message");
+  let image = document.querySelector("img.image");
+  let date = new Date();
+  let hour = 12;
+  let minute = date.getMinutes();
+
+  if (hour < 12) {
+    message.innerHTML = `Good Morning - `;
+    message.innerHTML += `It is ${hour}:${minute}`;
+    image.src = "morning.jpeg";
+    document.body.style.background = "#e2cd9f";
+  } else if (hour < 18) {
+    message.innerHTML = `Good Afternoon - `;
+    message.innerHTML += `It is ${hour}:${minute}`;
+    image.src = "afternoon.jpeg";
+    document.body.style.background = "#b9846f";
+  } else {
+    message.innerHTML = `Good Night - `;
+    message.innerHTML += `It is ${hour}:${minute}`;
+    image.src = "night.jpeg";
+    document.body.style.background = "#515154";
+  }
+}
